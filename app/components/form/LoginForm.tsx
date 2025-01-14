8791f4e9342ca25b0e381ffd0f136578f3a695b0
Jakub Grzymisławski <thegomel10@gmail.com>
2025-01-14 22:30:58 +0100

    Add simple styles to LoginForm, create ButtonPrimary component

--- app/components/form/LoginForm.tsx
+++ app/components/form/LoginForm.tsx
@@ -4,6 +4,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import Input from "../ui/Input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import z from "zod";
+import ButtonPrimary from "../ui/ButtonPrimary";
 
 const schema = z.object({
 	email: z.string().email("Invalid email address"),
@@ -22,28 +23,33 @@ const LoginForm = () => {
 	});
 
 	const onSubmit: SubmitHandler<FormFields> = (data) => {
-		console.log("test");
 		console.log(data);
 	};
 
 	return (
-		<div>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<Input
-					type="text"
-					inputProps={{ placeholder: "E-mail", ...register("email") }}
-				/>
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className="shadow-lg p-4 lg:p-6 flex flex-col max-w-md mx-auto bg-white gap-y-5"
+		>
+			<p className="text-center text-lg lg:text-xl">Log in to your account</p>
+			<div className="w-full">
+				<Input type="text" placeholder="E-mail" {...register("email")} />
 				{errors.email && <p className="text-red-500">{errors.email.message}</p>}
+			</div>
+			<div className="w-full">
 				<Input
 					type="password"
-					inputProps={{ placeholder: "Password", ...register("password") }}
+					placeholder="Password"
+					{...register("password")}
 				/>
 				{errors.password && (
 					<p className="text-red-500">{errors.password.message}</p>
 				)}
-				<button type="submit">Submit form</button>
-			</form>
-		</div>
+			</div>
+			<ButtonPrimary type="submit">Sign in</ButtonPrimary>
+			<hr className="h-px my-5 lg:my-6 bg-gray-500" />
+			<p className="text-center">Sign up</p>
+		</form>
 	);
 };
 
---
+++ app/components/ui/ButtonPrimary.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from "tailwind-merge";
+
+type ButtonPrimaryProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+	children: React.ReactNode;
+	className?: string;
+};
+
+const ButtonPrimary = ({
+	children,
+	className,
+	...buttonProps
+}: ButtonPrimaryProps) => {
+	return (
+		<button
+			className={twMerge(
+				"px-4 py-2 lg:px-6 lg:py-4 bg-blue-500 hover:bg-blue-400 transition-colors text-white rounded-md lg:rounded-lg disabled:opacity-60 disabled:cursor-not-allowed",
+				className
+			)}
+			{...buttonProps}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default ButtonPrimary;
